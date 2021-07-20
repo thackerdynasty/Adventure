990b54463eeb50d8725c3143cbe6a84e6860635a
thackerdynasty <70179471+thackerdynasty@users.noreply.github.com>
2021-07-19 21:12:05 -0400

    WIP(Work in progress)

--- main.py
+++ main.py
@@ -7,3 +7,21 @@ game_map = {
 	"alleyway":{"west":"home"}
 }
 descriptions = {"jail":"At the jailcell","sewers":"The sewers, main way to escape jail.","alleyway":"Dark and spooky, you cannot be spotted.","home":"Escaped!"}
+directions = [north, south, east, west]
+game_over = False
+def main():
+	global game_over
+	while not game_over:
+		ask = input("Which way do you want to go?")
+		if input is in directions:
+			print("hello!")
+		elif input == "look":
+			print("Ok")
+		elif input == "quit":
+			print("Goodbye")
+		elif input=="help":
+			print("enter north, south, east, west to move.")
+		else:
+			print("I'm afraid that this game cannot reconize that.")
+if __name__ == "__main__":
+	main()
