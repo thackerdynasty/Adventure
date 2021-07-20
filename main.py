c28db518185ca68231b4de63ef8335d10152c369
Supercoder245 <70179471+thackerdynasty@users.noreply.github.com>
2021-07-20 00:32:24 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+player = {"location":"jail"}
+game_map = {
+	"jail": {"south": "sewers"},
+	"sewers":{"west": "exit1", "south": "exit2"},
+	"exit1":{"south":"exit3","west": "bankvault", 
+	"north":"alleyway"},
+	"alleyway":{"west":"home"}
+}
+descriptions = {"jail":"At the jailcell","sewers":"The sewers, main way to escape jail."}
